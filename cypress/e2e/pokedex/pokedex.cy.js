675f38fecb0792f394866d173d7313c6e29d8e1c
Eche <echyth@yahoo.com>
2023-01-04 11:43:49 +0100

    write test for navigating to a pokemon page

--- cypress/e2e/pokedex/pokedex.cy.js
+++ cypress/e2e/pokedex/pokedex.cy.js
@@ -6,4 +6,10 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+
+  it("a specific pokemon page can be navigated to", function () {
+    cy.visit("http://localhost:5000");
+    cy.get("a.list-item").contains("ivysaur").click();
+    cy.contains("chlorophyll");
+  });
 });
