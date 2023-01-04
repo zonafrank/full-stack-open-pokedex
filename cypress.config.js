e87dec922e4c03ebf051bd0e782a240af36ebc3d
Eche <echyth@yahoo.com>
2023-01-04 10:52:31 +0100

    fix lint errors

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // eslint-disable-next-line
+    setupNodeEvents(_on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
